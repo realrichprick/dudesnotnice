69d9eec17c544e5bbd902ba7a04aca3e39f15689
realrichprick <nigger@easymails.cc>
2024-06-12 11:13:40 -0700

    Delete 6244--index.js

--- 6244--index.js
+++
@@ -1,7 +0,0 @@
-let titles = ['r...','ri...','rifl...','rifle...','rifl...','rif..','ri...','ri...','r...']
-let index = 0
-let interval = setInterval(() => {
-    document.title = titles[index]
-    index += 1
-    if (index === titles.length) index = 0
-}, 500)
